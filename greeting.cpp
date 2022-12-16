b3952aaa0ea68679362156bf3acaad6db250426e
Yasim <91527193+Yasimcd@users.noreply.github.com>
2022-12-16 15:03:43 -0800

    Update greeting.cpp

--- greeting.cpp
+++ greeting.cpp
@@ -1,13 +1,13 @@
 // greeting.cpp
 // This program prints a message to greet the user.
-#include <iostream>         // Needed to do C++ I/O
-#include <string>           // Needed by some compilers to use strings
+#include <iostream>  // Needed to do C++ I/O
+#include <string>   // Needed by some compilers to use strings
 using namespace std;
 
 int main ()
 {
-	string name;             // This declares a variable to
-                            // hold the user's name
+	string name;  // This declares a variable to hold the user's name
+                            
    // Get the user's name
    cout << "Please enter your first name: "; 
    cin  >> name;
